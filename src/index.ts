e5a8e7b1393f9662965aab04fc65778f424150da
Prince lohia <princelohia444@gmail.com>
2025-08-23 12:37:48 +0530

    2 client limit

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,23 @@ import express from "express";
 import { WebSocketServer, WebSocket } from "ws";
 
 const app = express();
-const httpServer = app.listen(8080);
+const httpServer = app.listen(8080, () => {
+  console.log("Server is listening on port 8080");
+});
 
 const wss = new WebSocketServer({ server: httpServer });
+const activeClients = new Set<WebSocket>();
 
 wss.on("connection", function connection(ws) {
+  if (activeClients.size >= 2) {
+    ws.close(1000, "Server limit of 2 clients reached");
+    ws.send("Connection closed: Server limit of 2 clients reached");
+    return; // Do not register further events for this client
+  }
+
+  activeClients.add(ws);
+  ws.send("Connected");
+
   ws.on("message", function message(data, isBinary) {
     wss.clients.forEach(function each(client) {
       if (client.readyState === WebSocket.OPEN) {
@@ -15,5 +27,7 @@ wss.on("connection", function connection(ws) {
     });
   });
 
-  ws.send("Hello! Message From Server!!");
+  ws.on("close", () => {
+    activeClients.delete(ws);
+  });
 });
